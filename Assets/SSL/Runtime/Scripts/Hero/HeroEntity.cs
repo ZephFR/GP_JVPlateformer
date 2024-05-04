33be9f7b5d8766500cf83ad4cf63cb7874236279
Maël <mael.perrot2005@gmail.com>
2024-05-04 15:03:43 +0200

    Commit 3 - Acceleration

--- Assets/SSL/Runtime/Scripts/Hero/HeroEntity.cs
+++ Assets/SSL/Runtime/Scripts/Hero/HeroEntity.cs
@@ -7,7 +7,8 @@ public class HeroEntity : MonoBehaviour
     [SerializeField] private Rigidbody2D _rigidbody;
 
     [Header("Horizontal Movements")]
-    [SerializeField] private float _horizontalSpeed = 5f;
+    [SerializeField] private HeroHorizontalMovementsSettings _movementsSettings;
+    private float _horizontalSpeed = 0f;
     private float _moveDirX = 0f;
     private float _orientX = 1f;
 
@@ -24,6 +25,7 @@ public class HeroEntity : MonoBehaviour
 
     private void FixedUpdate()
     {
+        _UpdateHorizontalSpeed();
         _ChangeOrientFromHorizontalMovement();
         _ApplyHorizontalSpeed();
     }
@@ -52,7 +54,27 @@ public class HeroEntity : MonoBehaviour
         newScale.x = _orientX;
         _orientVisualRoot.localScale = newScale;
     }
+    private void _Accelerate()
+    {
+        _horizontalSpeed += _movementsSettings.acceleration * Time.fixedDeltaTime;
+        if (_horizontalSpeed > _movementsSettings.speedMax)
+        {
+            _horizontalSpeed = _movementsSettings.speedMax;
+        }
+    }
 
+    private void _UpdateHorizontalSpeed()
+    {
+        if (_moveDirX != 0f)
+        {
+            _Accelerate();
+        }
+        else
+        {
+            _horizontalSpeed = 0f; 
+        }
+    }
+    
     private void OnGUI()
     {
         if (!_guiDebug) return;
---
+++ Assets/SSL/Runtime/Scripts/Hero/HeroHorizontalMovementsSettings.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public class HeroHorizontalMovementsSettings
+{
+    public float acceleration = 20f;
+    public float speedMax = 5f;
+}
