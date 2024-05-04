c0eb02ac11fc805204060ab5acba9df892d99d52
Maël <mael.perrot2005@gmail.com>
2024-05-04 15:55:15 +0200

    Commit 6 - Chute et Gravité

--- Assets/SSL/Runtime/Scripts/Hero/HeroEntity.cs
+++ Assets/SSL/Runtime/Scripts/Hero/HeroEntity.cs
@@ -18,10 +18,23 @@ public class HeroEntity : MonoBehaviour
     [Header("Debug")]
     [SerializeField] private bool _guiDebug = false;
 
+    [Header("Vertical Movements")]
+    private float _verticalSpeed = 0f;
+
+    [Header("Fall")]
+    [SerializeField] private HeroFallSettings _fallSettings;
+
+    #region Functions Move Dir
+    public float GetMoveDirX()
+    {
+        return _moveDirX;
+    }
     public void SetMoveDirX(float dirX)
     {
         _moveDirX = dirX;
     }
+
+    #endregion
 
     private void FixedUpdate()
     {
@@ -34,8 +47,26 @@ public class HeroEntity : MonoBehaviour
         _UpdateHorizontalSpeed();
         _ChangeOrientFromHorizontalMovement();
         }
-        
+        _ApplyFallGravity();
+
         _ApplyHorizontalSpeed();
+        _ApplyVerticalSpeed();
+    }
+
+    private void _ApplyVerticalSpeed()
+    {
+        Vector2 velocity = _rigidbody.velocity;
+        velocity.y = _verticalSpeed;
+        _rigidbody.velocity = velocity;
+    }
+
+    private void _ApplyFallGravity()
+    {
+        _verticalSpeed -= _fallSettings.fallGravity * Time.fixedDeltaTime;
+        if (_verticalSpeed < -_fallSettings.fallSpeedMax)
+        {
+            _verticalSpeed = -_fallSettings.fallSpeedMax;
+        }
     }
 
     private void _ChangeOrientFromHorizontalMovement()
@@ -116,6 +147,7 @@ public class HeroEntity : MonoBehaviour
         GUILayout.Label($"MoveDirX = {_moveDirX}");
         GUILayout.Label($"OrientX = {_orientX}");
         GUILayout.Label($"Horizontal Speed = {_horizontalSpeed}");
+        GUILayout.Label($"Vertical Speed = {_verticalSpeed}");
         GUILayout.EndVertical();
     }
 }
---
+++ Assets/SSL/Runtime/Scripts/Hero/HeroFallSettings.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+[Serializable]
+public class HeroFallSettings
+{
+    public float fallGravity = 20f;
+    public float fallSpeedMax = 10f;
+}
