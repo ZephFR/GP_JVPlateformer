b1325e67044db344aef2e17b1e86214857efe24f
Maël <mael.perrot2005@gmail.com>
2024-05-05 18:13:06 +0200

    Commit 14 - Camera Transition

--- Assets/SSL/Runtime/Scripts/Camera/CameraChangeProfileTrigger.cs
+++ Assets/SSL/Runtime/Scripts/Camera/CameraChangeProfileTrigger.cs
@@ -7,6 +7,12 @@ public class CameraChangeProfileTrigger : MonoBehaviour
     [Header("Camera Profile")]
     [SerializeField] private CameraProfile _cameraProfile;
     
+
+    [Header("Camera Transitions")]
+    [SerializeField] private CameraProfileTransition _enterTransition;
+    [SerializeField] private CameraProfileTransition _exitTransition;
+
+
     [Header("Trigger Box")]
     [SerializeField] private BoxCollider2D _boxCollider;
 
@@ -25,12 +31,12 @@ public class CameraChangeProfileTrigger : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag(TARGET_TAG)) return;
-        CameraManager.Instance.EnterProfile(_cameraProfile);
+        CameraManager.Instance.EnterProfile(_cameraProfile, _enterTransition);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         if (!other.CompareTag(TARGET_TAG)) return;
-        CameraManager.Instance.ExitProfile(_cameraProfile);
+        CameraManager.Instance.ExitProfile(_cameraProfile, _exitTransition);
     }
 }
--- Assets/SSL/Runtime/Scripts/Camera/CameraManager.cs
+++ Assets/SSL/Runtime/Scripts/Camera/CameraManager.cs
@@ -11,6 +11,10 @@ public class CameraManager : MonoBehaviour
     [Header("Profile System")]
     [SerializeField] private CameraProfile _defaultCameraProfile;
     private CameraProfile _currentCameraProfile;
+    private float _profileTransitionTimer = 0f;
+    private float _profileTransitionDuration = 0f;
+    private Vector3 _profileTransitionStartPosition;
+    private float _profileTransitionStartSize;
 
     private void Awake()
     {
@@ -44,21 +48,66 @@ public class CameraManager : MonoBehaviour
 
     private void Update()
     {
-        _SetCameraPosition(_currentCameraProfile.Position);
-        _SetCameraSize(_currentCameraProfile.CameraSize);
+        if (_IsPlayingProfileTransition()) 
+        {
+            _profileTransitionTimer += Time.deltaTime;
+            Vector3 transitionPosition = _CalculateProfileTransitionPosition(_currentCameraProfile.Position);
+            _SetCameraPosition(transitionPosition);
+            float transitionSize = _CalculateProfileTransitionCameraSize(_currentCameraProfile.CameraSize);
+            _SetCameraSize(transitionSize);
+        }
+        else
+        {
+            _SetCameraPosition(_currentCameraProfile.Position);
+            _SetCameraSize(_currentCameraProfile.CameraSize);
+        }
     }
 
-    public void EnterProfile(CameraProfile cameraProfile)
+    public void EnterProfile(CameraProfile cameraProfile, CameraProfileTransition transition = null)
     {
         _currentCameraProfile = cameraProfile;
+        if (transition != null)
+        {
+            _PlayProfileTransition(transition);
+        }
     }
 
-    public void ExitProfile(CameraProfile cameraProfile)
+    public void ExitProfile(CameraProfile cameraProfile, CameraProfileTransition transition = null)
     {
-        if ( _currentCameraProfile != cameraProfile) 
+        if (_currentCameraProfile != cameraProfile) return;
+        _currentCameraProfile = _defaultCameraProfile;
+        if (transition != null)
         {
-            if (_currentCameraProfile != cameraProfile) return;
-            _currentCameraProfile = _defaultCameraProfile;
+            _PlayProfileTransition(transition);
         }
+    }
+
+    private void _PlayProfileTransition(CameraProfileTransition transition)
+    {
+        _profileTransitionStartPosition = _camera.transform.position;
+        
+        _profileTransitionStartSize = _camera.orthographicSize;
+
+        _profileTransitionTimer = 0f;
+        _profileTransitionDuration = transition.duration;
+    }
+
+    private bool _IsPlayingProfileTransition()
+    {
+        return _profileTransitionTimer < _profileTransitionDuration;
+    }
+
+    private float _CalculateProfileTransitionCameraSize(float endSize)
+    {
+        float percent = _profileTransitionTimer / _profileTransitionDuration;
+        float startSize = _profileTransitionStartSize;
+        return Mathf.Lerp(startSize, endSize, percent);
+    }
+
+    private Vector3 _CalculateProfileTransitionPosition(Vector3 destination)
+    {
+        float percent = _profileTransitionTimer / _profileTransitionDuration;
+        Vector3 origin = _profileTransitionStartPosition;
+        return Vector3.Lerp(origin, destination, percent);
     }
 }
