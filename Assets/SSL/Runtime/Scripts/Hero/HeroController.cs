79dc0adfac3c8acbf1ddbb17362fcd220760564f
Maël <mael.perrot2005@gmail.com>
2024-05-05 15:35:50 +0200

    Commit 11 - Air Control

--- Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
+++ Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
@@ -1,3 +1,4 @@
+using Unity.Collections;
 using UnityEngine;
 
 public class HeroController : MonoBehaviour
@@ -8,20 +9,39 @@ public class HeroController : MonoBehaviour
     [Header("Debug")]
     [SerializeField] private bool _guiDebug = false;
 
+    [Header("Jump Buffer")]
+    [SerializeField] private float _jumpBufferDuration = 0.2f;
+    private float _jumpBufferTimer = 0f;
+
     private void OnGUI()
     {
         if (!_guiDebug) return;
 
         GUILayout.BeginVertical(GUI.skin.box);
         GUILayout.Label(gameObject.name);
+        GUILayout.Label($"Jump Buffer Timer = {_jumpBufferTimer}");
         GUILayout.EndVertical();
     }
 
     private void Update()
     {
+        _UpdateJumpBuffer();
+
         _entity.SetMoveDirX(GetInputMoveX());
 
         if (_GetInputDownJump())
+        {
+            if (_entity.IsTouchingGround && !_entity.IsJumping)
+            {
+                _entity.JumpStart();
+            }
+            else
+            {
+                _ResetJumpBuffer();
+            }
+        }
+
+        if (IsJumpBufferActive())
         {
             if (_entity.IsTouchingGround && !_entity.IsJumping)
             {
@@ -62,5 +82,26 @@ public class HeroController : MonoBehaviour
     private bool _GetInputJump() 
     {
         return Input.GetKey(KeyCode.Space);
+    }
+
+    private void _ResetJumpBuffer()
+    {
+        _jumpBufferTimer = 0f;
+    }
+
+    private bool IsJumpBufferActive()
+    {
+        return _jumpBufferTimer < _jumpBufferDuration;
+    }
+
+    private void _UpdateJumpBuffer()
+    {
+        if (!IsJumpBufferActive()) return;
+        _jumpBufferTimer += Time.deltaTime;
+    }
+
+    private void _CancelJumpBuffer()
+    {
+        _jumpBufferTimer = _jumpBufferDuration;
     }
 }
